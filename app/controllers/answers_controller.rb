ccd90e7dcd9f8f3dfd1bf5e706a3e11fac4a9a0e
Joseph Kim <email": null>
2016-03-04 14:23:20 -0500

    3pm commit before pulling from master

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,36 +1,66 @@
 class AnswersController < ApplicationController
 
   def new
-    @answer = Answer.new
+    question = Question.find(params[:question_id])
+    @answer = question.answers.new
   end
 
   def create
-    @answer = Answer.new(answer_params)
-    @answer.user_id = 1
-    if @answer.save
-      redirect_to @answer
+    if logged_in?
+      question = Question.find(params[:question_id])
+      @answer = question.answers.new(answer_params)
+      @answer.user_id = session[:user_id]
+      @answer.question_id = question.id
+      if @answer.save
+        redirect_to question_path(question)
+      else
+        # @errors = @answer.errors.full_messages
+        render :new
+      end
     else
-      @errors = @answer.errors.full_messages
-      render :new
+      redirect_to login_path
     end
   end
 
-  def show
-    @answer = Answer.find(params[:id])
-  end
-
   def edit
+    @answer = Answer.find(params[:id])
+    unless @answer.user_id == session[:user_id]
+      redirect_to login_path
+    end
   end
 
   def update
+    find_question_for_answer
+    if logged_in? && @answer.user.id == current_user.id
+      if @answer.update(answer_params)
+        redirect_to question_path(@question)
+      else
+        # @errors = @answer.errors.full_messages
+        render :edit
+      end
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
+    find_question_for_answer
+    if logged_in? &&& @answer.user.id == current_user.id
+      @answer.destroy
+      redirect_to question_path(@question)
+    else
+      redirect_to login_path
+    end
   end
 
   private
 
   def answer_params
     params.require(:answer).permit(:body)
+  end
+
+  def find_question_for_answer
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resource :sessions, only: [:create]
   resource :users, only: [:create]
+  resource :answer, except: [:show]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
