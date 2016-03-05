c36054fc93430e3dcfdfa02a98f9d95f74990005
Liam Mackey <liammac21@gmail.com>
2016-03-05 17:25:36 -0500

    Did some more refactoring

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,10 +3,10 @@ class VotesController < ApplicationController
     if logged_in?
       if params[:question_id]
         @question = Question.find(params[:question_id])
-        create_question_votes(@question)
+        create_votes(@question)
       else
         @answer = Answer.find(params[:answer_id])
-        create_answer_votes(@answer)
+        create_votes(@answer)
       end
     else
       redirect_to login_path
@@ -14,21 +14,16 @@ class VotesController < ApplicationController
   end
 
   private
-  def create_answer_votes(value)
+  def create_votes(value)
     if value.has_not_been_rated_by_user(current_user)
       vote = value.votes.new(direction_value: params[:vote][:direction_value])
       vote.user = current_user
       vote.save
     end
-    redirect_to value.question
-  end
-
-  def create_question_votes(value)
-    if value.has_not_been_rated_by_user(current_user)
-      vote = value.votes.new(direction_value: params[:vote][:direction_value])
-      vote.user = current_user
-      vote.save
+    if value.is_a?(Answer)
+      redirect_to value.question
+    else
+      redirect_to value
     end
-    redirect_to value
   end
 end
