951190d36efd426f4d42daee5c4a069793250306
Joseph Kim <email": null>
2016-03-06 18:31:30 -0500

    add seed files for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+questionoranswer = ["Question", "Answer"]
+votevalue = [-1,1,1]
+
+5.times do |i|
+  User.create(username: Faker::Internet.user_name, password: "password")
+end
+
+25.times do |i|
+  Question.create(title: Faker::Hipster.sentence, body: Faker::Hipster.paragraph(rand(1..5)), user_id: rand(1..5))
+end
+
+50.times do |i|
+  Answer.create(body: Faker::Hipster.paragraph(rand(1..3)), user_id: rand(1..5), question_id: rand(1..25))
+end
+
+100.times do |i|
+  Comment.create(body: Faker::Hipster.paragraph(rand(1..3)), user_id: rand(1..5), commentable_type: questionoranswer.sample, commentable_id: rand(1..25))
+end
+
+500.times do |i|
+  Vote.create(direction_value: votevalue.sample, user_id: rand(1..5), votable_type: questionoranswer.sample, votable_id: rand(1..25))
+end
